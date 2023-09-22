22a24e809bc943eebbb33e90d3e6025252743117
Michael Duncan <81819246+MDuncan98@users.noreply.github.com>
2023-09-22 15:27:32 +0100

    Revert "[BR-3539] Fuzzy search"
    This reverts commit 95a6f9df1ede8b225e01a570d5b99757ad0d74a0.

--- Gibe.Umbraco.Blog/Filters/SearchTermBlogPostFilter.cs
+++ Gibe.Umbraco.Blog/Filters/SearchTermBlogPostFilter.cs
@@ -14,7 +14,7 @@ namespace Gibe.Umbraco.Blog.Filters
 
 		public IBooleanOperation GetCriteria(IQuery query)
 		{
-			return query.Field(ExamineFields.BodyText, SearchTerm.Fuzzy());
+			return query.Field(ExamineFields.BodyText, SearchTerm.MultipleCharacterWildcard());
 		}
 	}
 }
