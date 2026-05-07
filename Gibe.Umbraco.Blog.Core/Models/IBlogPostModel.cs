ecba6686d1cb2a716abd0a47c9e308b494755116
Michael Duncan <81819246+MDuncan98@users.noreply.github.com>
2026-05-07 12:10:34 +0100

    [BR-5446] Split Interfaces into Gibe.Umbraco.Blog.Core library

--- Gibe.Umbraco.Blog.Core/Models/IBlogPostModel.cs
+++ Gibe.Umbraco.Blog.Core/Models/IBlogPostModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Umbraco.Cms.Core.Models.PublishedContent;
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace Gibe.Umbraco.Blog.Models
 {
