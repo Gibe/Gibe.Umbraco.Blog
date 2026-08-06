00695beae659dbbc90e1373f44597110d201dff9
Steve Temple <steve@gibedigital.com>
2026-08-06 10:24:10 +0100

    Fix BlogTags

--- Gibe.Umbraco.Blog/BlogTags.cs
+++ Gibe.Umbraco.Blog/BlogTags.cs
@@ -9,50 +9,41 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace Gibe.Umbraco.Blog
 {
-	public class BlogTags : IBlogTags
-	{
-		private readonly IBlogSearch _blogSearch;
+    public class BlogTags : IBlogTags
+    {
+        private readonly IBlogSearch _blogSearch;
         private readonly string _propertyName = ExamineFields.Tags;
 
 
         public BlogTags(IBlogSearch blogSearch)
-		{
-			_blogSearch = blogSearch;
-		}
+        {
+            _blogSearch = blogSearch;
+        }
 
-		public IEnumerable<BlogTag> All(IPublishedContent blogRoot)
-		{
-			var allTags = new Dictionary<string, BlogTag>();
-			var posts = _blogSearch.Search(new SectionBlogPostFilter(blogRoot.Id ), new DateSort());
+        public IEnumerable<BlogTag> All(IPublishedContent blogRoot)
+        {
+            var allTags = new Dictionary<string, BlogTag>();
+            var posts = _blogSearch.Search(new SectionBlogPostFilter(blogRoot.Id), new DateSort());
 
-			var applicablePosts = posts.Where(post => post.Values.ContainsKey($"{_propertyName}") && !string.IsNullOrEmpty(post.Values[$"{_propertyName}"]))
-				.SelectMany(post => ParseTags(post.Values[$"{_propertyName}"]));
+            var applicablePosts = posts.Where(post => post.Values.ContainsKey($"{_propertyName}") && !string.IsNullOrEmpty(post.Values[$"{_propertyName}"]));
+            var tagSets = applicablePosts.Select(posts => posts.AllValues[$"{_propertyName}"]);
 
-			foreach (var tag in applicablePosts)
-			{
-				if (allTags.TryGetValue(tag, out var allTag))
-				{
-					allTag.Count++;
-				}
-				else
-				{
-					allTags.Add(tag, new BlogTag { Count = 1, Tag = tag, Url = $"{blogRoot.Url()}?{ExamineFields.Tag}={tag}"});
-				}
-			}
-			return allTags.Values;
-		}
-
-		private static IEnumerable<string> ParseTags(string rawValue)
-		{
-			try
-			{
-				return JsonConvert.DeserializeObject<IEnumerable<string>>(rawValue) ?? Enumerable.Empty<string>();
-			}
-			catch (JsonReaderException)
-			{
-				// Tags are likely csv rather than JSON
-				return rawValue.Split(',');
-			}
-		}
-	}
+            foreach (var tagSet in tagSets)
+            {
+                foreach (var tag in tagSet)
+                {
+                    if (allTags.TryGetValue(tag, out var allTag))
+                    {
+                        allTag.Count++;
+                    }
+                    else
+                    {
+                        allTags.Add(tag,
+                            new BlogTag { Count = 1, Tag = tag, Url = $"{blogRoot.Url()}?{ExamineFields.Tag}={tag}" });
+                    }
+                }
+            }
+            return allTags.Values.OrderByDescending(t => t.Count);
+        }
+    }
 }
